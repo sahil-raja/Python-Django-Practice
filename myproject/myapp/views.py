b1420cda6f4f8cd3579051ca4d7daac209cfad25
sahil-raja <sahilrp3@gmail.com>
2023-05-31 20:01:53 +0500

    simple word count application created

---
+++ myproject/myapp/views.py
@@ -0,0 +1,19 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+
+
+def index(request):
+    context = {
+        'name': 'John',
+        'age': 20,
+        'nationality': 'Nigerian'
+    }
+    return render(request, 'index.html', context)
+
+
+def counter(request):
+    text = request.GET['text']
+    total_count_words = len(text.split(' '))
+    return render(request, 'counter.html', {'count': total_count_words})
