319512fd0ef51a2b4806d6e71bdcd03d1018f5bf
sahil-raja <sahilrp3@gmail.com>
2023-06-01 12:32:41 +0500

    user authentication and registration

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('counter', views.counter, name='counter')
+    path('counter', views.counter, name='counter'),
+    path('register', views.register, name='register'),
+    path('login', views.login, name='login'),
+    path('logout', views.logout, name='logout')
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 from django.http import HttpResponse
 from .models import Feature
 
@@ -46,3 +48,49 @@ def counter(request):
     text = request.POST['text']
     total_count_words = len(text.split(' '))
     return render(request, 'counter.html', {'count': total_count_words})
+
+
+def register(request):
+    if request.method == 'POST':
+        uname = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['confirmation']
+
+        if password == password2:
+            if User.objects.filter(username=uname).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(
+                    username=uname, email=email, password=password)
+                user.save()
+                return redirect('login')
+        else:
+            messages.info(request, 'Password not matching')
+            return redirect('register')
+    return render(request, 'register.html')
+
+
+def login(request):
+    if request.method == 'POST':
+        uname = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=uname, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Invalid Credentials')
+            return redirect('login')
+    return render(request, 'login.html')
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
